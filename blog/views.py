e1530bd59f3974823c3e30634053db61f888705c
Aswini R Nair <aswinirnair312@gmail.com>
2024-03-25 00:18:11 +0530

    change content

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,14 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+import math
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    read_time = post.calculate_read_time()
+    return render(request, 'blog/post_detail.html', {'post': post, 'read_time': read_time})
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
